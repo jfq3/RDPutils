ce4f46264f492d72bad796fab8d447a14849f570
jfq3 <quensenj@msu.edu>
2017-01-14 13:51:37 -0500

    Added function to import USEARCH biom file.

---
+++ R/import_usearch_biom_file.R
@@ -0,0 +1,8 @@
+import_usearch_biom_file <- function(in_file, ...) {
+  parse_taxonomy_usearch <- function (char.vec){
+    parse_taxonomy_default(strsplit(char.vec, ",", TRUE)[[1]])
+  }
+  expt <- import_biom(in_file, parseFunction = parse_taxonomy_usearch, ...)
+  return(expt)
+}
+
