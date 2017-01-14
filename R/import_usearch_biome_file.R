0629b2e991de5eea6a6139dcff35c05d7974abfc
jfq3 <quensenj@msu.edu>
2017-01-14 11:36:38 -0500

    Added function to import USEARCH biom files.

--- R/import_usearch_biome_file.R
+++
@@ -1,8 +0,0 @@
-import_usearch_biome_file <- function(in_file, ...) {
-  parse_taxonomy_usearch <- function (char.vec){
-    parse_taxonomy_default(strsplit(char.vec, ",", TRUE)[[1]])
-  }
-  expt <- import_biom(in_file, parseFunction = parse_taxonomy_usearch, ...)
-  return(expt)
-}
-
