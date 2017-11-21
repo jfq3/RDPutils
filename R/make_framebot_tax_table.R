c2806cd57bd1d3c654e6b59c22da3b991be3f9e2
jfq3 <quensenj@msu.edu>
2017-11-21 17:24:33 -0500

    Add make_framebot_tax_table

---
+++ R/make_framebot_tax_table.R
@@ -0,0 +1,15 @@
+make_framebot_tax_table <- function(clstr_machine="match_cluster_machine_name.txt", taxa_machine="match_taxa_machine_names.txt") {
+  clstr_machine <- read.table(file="match_cluster_machine_name.txt", header = FALSE, stringsAsFactors = FALSE, sep="\t")
+  taxa_machine <- read.table(file="match_taxa_machine_names.txt", header = FALSE, stringsAsFactors = FALSE, sep="\t")
+  colnames(taxa_machine) <- c("strain","machine_name", "nucl_length", "as_length", "%identity")
+  colnames(clstr_machine) <- c("cluster", "machine_name")
+  taxonomy_table <- merge(clstr_machine, taxa_machine, by.x="machine_name", by.y = "machine_name")
+  row.names(taxonomy_table) <- taxonomy_table[,"cluster"]
+  for (i in 1:nrow(taxonomy_table)) {
+    taxonomy_table[i,"genus"] <- strsplit(taxonomy_table[i,"strain"],"_")[[1]][1]
+    taxonomy_table[i,"species"] <- strsplit(taxonomy_table[i,"strain"],"_")[[1]][2]
+  }
+  taxonomy_table <- taxonomy_table[ , c("genus", "species", "strain")]
+  taxonomy_table <- tax_table(as.matrix(taxonomy_table), errorIfNULL = TRUE)
+  return(taxonomy_table)
+}
