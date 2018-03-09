570faa0a25e5afc597985ab0410ed1a9816c5630
John Quensen <quensenj@msu.edu>
2018-03-09 09:00:11 -0500

    Add % match to FrambeBot tax table

--- R/make_framebot_tax_table.R
+++ R/make_framebot_tax_table.R
@@ -1,7 +1,8 @@
 make_framebot_tax_table <- function(clstr_machine="match_cluster_machine_name.txt", taxa_machine="match_taxa_machine_names.txt") {
-  clstr_machine <- read.table(file="match_cluster_machine_name.txt", header = FALSE, stringsAsFactors = FALSE, sep="\t")
-  taxa_machine <- read.table(file="match_taxa_machine_names.txt", header = FALSE, stringsAsFactors = FALSE, sep="\t")
+  clstr_machine <- read.table(file=clstr_machine, header = FALSE, stringsAsFactors = FALSE, sep="\t")
+  taxa_machine <- read.table(file=taxa_machine, header = FALSE, stringsAsFactors = FALSE, sep="\t")
   colnames(taxa_machine) <- c("strain","machine_name", "nucl_length", "as_length", "%identity")
+  taxa_machine[ , 1] <- paste(taxa_machine[ , 1], taxa_machine[ , 5], sep = "_%")
   colnames(clstr_machine) <- c("cluster", "machine_name")
   taxonomy_table <- merge(clstr_machine, taxa_machine, by.x="machine_name", by.y = "machine_name")
   row.names(taxonomy_table) <- taxonomy_table[,"cluster"]
