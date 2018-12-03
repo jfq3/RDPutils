07dda0ce1ddde1c346ccecb12ba3e4aef4cef6ba
John Quensen <quensenj@msu.edu>
2018-12-03 07:35:54 -0500

    Add reformat_taxa_names

---
+++ R/reformat_taxa_names.R
@@ -0,0 +1,14 @@
+#' Reformat Taxa Names
+#'
+#' @param p A phyloseq object including taxa
+#' @param otu_format A string specifying format
+#' @details Format string is either "R" (the default) or "biom", corresponding to the format returned by RDPTools.
+#' 
+#' @return A phyloseq object with reformatted taxa names
+#' @export
+#'
+#' @examples
+reformat_taxa_names <- function(p, otu_format = "R") {
+  taxa_names(p) <- make_otu_names(get_digits(taxa_names(p)), otu_format)
+  return(p)
+}
