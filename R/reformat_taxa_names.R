9c8587bbef8b57aefbc677c8ce29f612f5077159
John Quensen <quensenj@msu.edu>
2018-12-03 07:40:25 -0500

    Removed example

--- R/reformat_taxa_names.R
+++ R/reformat_taxa_names.R
@@ -6,8 +6,8 @@
 #' 
 #' @return A phyloseq object with reformatted taxa names
 #' @export
-#'
-#' @examples
+
+
 reformat_taxa_names <- function(p, otu_format = "R") {
   taxa_names(p) <- make_otu_names(get_digits(taxa_names(p)), otu_format)
   return(p)
