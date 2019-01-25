2fa27b4a6c528f96b7d93f02e6ee2f95778a60c3
John Quensen <quensenj@msu.edu>
2019-01-25 09:05:45 -0500

    edidt spelling

--- R/reformat_taxa_names.R
+++ R/reformat_taxa_names.R
@@ -2,7 +2,7 @@
 #'
 #' @param p A phyloseq object including taxa
 #' @param otu_format A string specifying format
-#' @details Format string is either "R" (the default) or "biom", corresponding to the format returned by RDPTools.
+#' @details Format string is either "R" (the default) or "biom", corresponding to the formats returned by RDPTools.
 #' 
 #' @return A phyloseq object with reformatted taxa names
 #' @export
