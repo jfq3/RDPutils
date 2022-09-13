e9a1bbd5516a4370bd0207b30d806cdd3ec50819
John Quensen <quensenj@msu.edu>
2022-09-13 12:25:47 -0400

    Started roxygenation of package with simple_cap function

--- R/simple_cap.R
+++ R/simple_cap.R
@@ -1,3 +1,14 @@
+#' Simple Capitalization
+#' @aliases simple_cap
+#' @usage simple_cap(x)
+#' @param x A string or vector of strings
+#'
+#' @return The string with every word capitalized
+#' @export
+#'
+#' @examples
+#' x <- "The quick brown fox jumped over the lazy dog."
+#' simple_cap(x)
 simple_cap <- function(x) {
   s <- strsplit(x, " ")[[1]]
   paste(toupper(substring(s, 1,1)), substring(s, 2),
