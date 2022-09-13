408d8f2a6829ba0fb26ae0ad4f97b2d673a9ad8a
John Quensen <quensenj@msu.edu>
2022-09-13 15:23:12 -0400

    Roxygenize count_char_occurrences function

--- R/count_char_occurrences.R
+++ R/count_char_occurrences.R
@@ -1,6 +1,16 @@
-# s is a string
-# Char is a character to search for in the string
-# Returns the number of times char occurs in strng_x.
+#' @title  Count Occurrences of a Character
+#' @name count_char_occurrences
+#' @aliases count_char_occurrences
+#' @export
+#' @description Count the number of occurrences of a character in a string.
+#' @usage count_char_occurrences(char, strng_x)
+#' @param char Character to search for
+#' @param strng_x String that is searched
+#' @returns An integer, the number of times a character occurs in a string.
+#' @author John Quensen
+#' @examples 
+#' my.strng <- "abcdefabcaa"
+#' count_char_occurrences("a", strng_x=my.strng)
 
 count_char_occurrences <- function(char, strng_x) {
   s2 <- gsub(char,"",strng_x)
