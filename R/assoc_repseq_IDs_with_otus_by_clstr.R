9601ee12529e859ae6861d535048ac70ed624563
John Quensen <quensenj@msu.edu>
2022-09-13 13:06:16 -0400

    Roxygenized assoc_repseq_ISs_with_otus_by_clstr and get_repseq_IDs_from_fasta

--- R/assoc_repseq_IDs_with_otus_by_clstr.R
+++ R/assoc_repseq_IDs_with_otus_by_clstr.R
@@ -1,3 +1,24 @@
+#' Associate Representative Sequence IDs with OTUs from Cluster File
+#' assoc_repseq_IDs_with_otus_by_clstr
+#' @description This function parses a cluster file for a single distance and makes a table associating the representative sequence machine names with the OTU names as given by RDP's cluster file formatter with options "R" or "biom."
+#' @param clstr_file The name of a cluster file for a single distance.
+#' @param rep_seqs A vector of representative sequence machine names.
+#' @param otu_format When equal to "R" (default) OTU names have the form "OTUxxxnn."  When equal to "biom", OTU names have the form "cluster_nn."
+#' @details 
+#' The first input to this function is the name of a cluster file for a single distance. The function reads the cluster file from disk. The cluster file for a single distance is excised from the original cluster file which likely contained cluster information for several distances with either of the functions clstr2otu or split_clstr_file. 
+#' The second input to this function is a vector of representative sequence machine names corresponding to the input cluster file; that is, they are for the same distance.  This vector is created from a fasta file of the representative sequences with the function get_repseq_IDs_from_fasta. 
+#' OTUs may be named in either of two formats, corresponding to those output by the RDP's cluster file formatter with options "R" and "biom."  With option "R" (the default), OTU names begin with "OTU" and are padded to equal length with leading zeros, e.g. "OTU00067."  Thus they can be sorted in numerical order.  With option "biom," OTU names begin with "cluster_" and are not padded with leading zeros, e.g "cluster_67."
+#' @usage assoc_repseq_IDs_with_otus_by_clstr(clstr_file, rep_seqs, otu_format)
+#' @author John Quensen
+#' @references The web-based tool for retrieving representative sequences is here: http://pyro.cme.msu.edu/   The RDPTools are available on GitHub:  https://github.com/rdpstaff
+#' @examples 
+#' repseq.file <- system.file("extdata", "all_seq_complete.clust_rep_seqs.fasta", package="RDPutils")
+#' rep.seqs <- get_repseq_IDs_from_fasta(repseq_file = repseq.file)
+#' clstr.file <- system.file("extdata", "dist_03.clust", package="RDPutils")
+#' assoc.table <- assoc_repseq_IDs_with_otus_by_clstr(clstr_file = clstr.file, rep_seqs = rep.seqs)
+#' head(assoc.table)
+#' @keywords RDPTools
+
 assoc_repseq_IDs_with_otus_by_clstr <-
 function(clstr_file, rep_seqs, otu_format="R") {
   clstr.file <- read.table(file=clstr_file, stringsAsFactors=FALSE, header=FALSE,  sep="\t", fill=TRUE)
--- R/get_repseq_IDs_from_fasta.R
+++ R/get_repseq_IDs_from_fasta.R
@@ -1,3 +1,24 @@
+#' @title Get Representative Sequence Names
+#' @name get_repseq_IDs_from_fasta
+#' @aliases get_repseq_IDs_from_fasta
+#' @description This function retrieves a vector of the representative sequence machine names from a fasta file of representative sequences.
+#' @usage get_repseq_IDs_from_fasta(repseq_file = "all_seq_complete.clust_rep_seqs.fasta")
+#' @param repseq_file The file name of the combined fasta file containing the representative sequences.
+#' @details The function reads the fasta file of representative sequences from disk.
+#' @return A vector of the machine names of the representative sequences.
+#' @references 
+#' The web-based tool for retrieving representative sequences is here:
+#' http://pyro.cme.msu.edu/
+#'  
+#' A command line version of the tool is included in RDPTools, available on GitHub:
+#' https://github.com/rdpstaff
+#' @author John Quensen
+#' @examples 
+#' repseq.file <- system.file("extdata", "all_seq_complete.clust_rep_seqs.fasta", package="RDPutils")
+#' rep.seqs <- get_repseq_IDs_from_fasta(repseq_file = repseq.file)
+#' rep.seqs
+#' @keywords RDPTols
+
 get_repseq_IDs_from_fasta <-
 function(repseq_file="all_seq_complete.clust_rep_seqs.fasta") {
   # Read in fasta file as a text file.
