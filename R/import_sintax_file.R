1a62650527633255072100a8d9cd30085036a220
John Quensen <quensenj@msu.edu>
2022-09-15 12:39:45 -0400

    import_sintax_file

--- R/import_sintax_file.R
+++ R/import_sintax_file.R
@@ -1,3 +1,20 @@
+#' @title Import sintax Taxonomy File
+#' @name import_sintax_file
+#' @aliases import_sintax_file
+#' @description Converts the tab-delimited output of USEARCH's (version 9+) sintax command to a phyloseq tax_table object. The confidence level for taxonomic assignment is chosen on import.
+#' @usage import_sintax_file(in_file, confidence = 0.8)
+#' @param in_file A sintax file
+#' @param confidence The confidence level to use in assigning taxonomic categories
+#' @details The sintax algorithm is new to USEARCH 9. It does not require training and still calculates the confidence with which taxonomic assignments are made. It does this by a method purported to be more accurate than the RDP classifier method. This function allows choice of the confidence level (0.8 by default) at the time the file is imported as a phyloseq tax_table.
+#' @returns A phyloseq tax_table.
+#' @references Edgar, R.C. (2016), SINTAX, a simple non-Bayesian taxonomy classifier for 16S and ITS sequences, http://dx.doi.org/10.1101/074161.
+#' @author John Quensen
+#' @export
+#' @examples 
+#' ##---- Not run. ----
+#' ##-- import_sintax_file(in_file="sintax_result.txt", confidence = 0.8)
+#' @keywords USEARCH
+
 import_sintax_file <- function (in_file = "sintax_file.txt", confidence = 0.8) {
   # Read in sintax file.
   temp <- read.table(file = in_file, sep = "\t", fill = TRUE, stringsAsFactors = FALSE)
