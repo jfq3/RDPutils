b28b8dbf2d9d12f38e48d965167696b5401544e0
John Quensen <quensenj@msu.edu>
2022-09-15 21:57:53 -0400

    Roxygenized make_otu_names

--- R/make_otu_names.R
+++ R/make_otu_names.R
@@ -1,3 +1,20 @@
+#' @title Make OTU Names
+#' @name make_otu_names
+#' @aliases make_otu_names
+#' @description This function takes a vector of integers and makes a vector of OTU names.
+#' @usage make_otu_names(otu_nums, otu_format="R")
+#' @param otu_nums A vector of integers.
+#' @param otu_format When equal to "R" (default) OTU names have the form "OTUxxxnn."  When equal to "biom", OTU names have the form "cluster_nn."
+#' @details This function is used by the function clstr2otu to name the columns of the OTU table it returns, but make_otu_names can be used separately if desired.
+#' @returns A vector of character strings of the form "OTU_xxxnn"" or "cluster_nn."
+#' @author John Quensen
+#' @export
+#' @examples 
+#' n <- c(1:10)
+#' otu.names <- make_otu_names(n)
+#' otu.names
+#' @keywords RDPTools
+
 make_otu_names <-
 function(otu_nums, otu_format="R") {
   new.clstr.name <- matrix("",length(otu_nums),1)
