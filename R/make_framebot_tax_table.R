daf3f6e84f40836d689fe2185f929587905933e6
John Quensen <quensenj@msu.edu>
2022-09-15 21:43:57 -0400

    Roxygenized import_utax_file

--- R/make_framebot_tax_table.R
+++ R/make_framebot_tax_table.R
@@ -1,3 +1,19 @@
+#' @title Import utax Taxonomy File
+#' @name import_utax_file
+#' @aliases import_utax_file
+#' @description Converts the tab-delimited output of USEARCH's utax command to a phyloseq tax_table object. The confidence level for taxonomic assignment is chosen on import.
+#' @usage import_utax_file(in_file, confidence)
+#' @param in_file The utaxout result from USEARCH's utax command.
+#' @param confidence The confidence level to use in assigning taxonomic categories.
+#' @details The default confidence level is 0.8.
+#' @returns A phyloseq tax_table.
+#' @author John Quensen
+#' @export
+#' @examples 
+#' ##---- Not run. ----
+#' ##-- tax.table <- import_utax_file(in_file="utax_result.txt", confidence = 0.8)
+#' @keywords USEARCH
+
 make_framebot_tax_table <- function(clstr_machine="match_cluster_machine_name.txt", taxa_machine="match_taxa_machine_names.txt") {
   clstr_machine <- read.table(file=clstr_machine, header = FALSE, stringsAsFactors = FALSE, sep="\t")
   taxa_machine <- read.table(file=taxa_machine, header = FALSE, stringsAsFactors = FALSE, sep="\t")
