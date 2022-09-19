3b5232c562713cd8457890bcaf4872026f0f7477
John Quensen <quensenj@msu.edu>
2022-09-18 22:03:02 -0400

    Roxygenize select_seqs

--- R/select_seqs.R
+++ R/select_seqs.R
@@ -1,6 +1,23 @@
-# Function select_seqs
-# Reads in a fasta file and writes out a subset of that fasta file.
-# Names of sequences in the output file are given in list select_list.
+#' @title Select Fasta Sequences
+#' @name select_seqs
+#' @aliases select_seqs
+#' @description This function subsets a combined fasta file.
+#' @usage select_seqs(in_file, select_list, out_file)
+#' @param in_file The name of a combined fasta file to be read from disk.
+#' @param select_list A vector of the names of the individual fasta files to be kept.
+#' @param out_file The name of the modified combined fasta file to be written to disk.
+#' @details This function can be used to select a subset of the renamed representative sequences corresponding to OTUs containing at least n sequences.  See the example section below.
+#' Reducing the number of representative sequences in this manner makes several subsequent steps go faster: classifying the representative sequences, making a phyloseq tax_table, and treeing the representative sequences.
+#' @returns This function operates on disk files.  It is not normally assigned to a variable.  It returns a message that it has completed.
+#' @author John Quensen
+#' @export
+#' @examples 
+#' renamed.fasta <- system.file("extdata", "renamed.fasta", package="RDPutils")
+#' data(otu)
+#' otu <- otu[ , colSums(otu)>=5]
+#' select.list <- colnames(otu)
+#' select_seqs(in_file=renamed.fasta, select_list=select.list, out_file="subset.renamed.repseqs.fasta")
+#' @keywords RDptools
 
 select_seqs = function(in_file, select_list, out_file) {
   con_out <- file(out_file, "a")
