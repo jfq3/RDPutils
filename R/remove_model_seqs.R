5562792493d6d739eeca3c092eb98aed304477d5
John Quensen <quensenj@msu.edu>
2022-09-18 21:50:32 -0400

    Roxygenize remove_model_seqs

--- R/remove_model_seqs.R
+++ R/remove_model_seqs.R
@@ -1,3 +1,20 @@
+#' @title Remove Model Sequences
+#' @name remove_model_seqs
+#' @aliases remove_model_seqs
+#' @description This function removes model sequences from a combined fasta file.
+#' @usage remove_model_seqs(in_file, out_file = in_file)
+#' @param in_file The name of a combined fasta file with model sequences.
+#' @param out_file The name of the corresponding fasta file written to disk without model sequences.
+#' @details This function operates on files.  It is not normally assigned to a variable.  By default, the input file is overwritten.  If no model sequence is found in the input file, a message to that effect is returned.
+#' RDP aligns 16S rRNA gene sequences using the Infernal aligner with model sequences for Bacteria or Archaea.  A model sequence is introduced into each alignment.  These model sequences are required by the cluster tool and the merge alignment tool.  If a model sequence is present in the fasta file of representative sequences, it needs to be removed before the sequences can be treed with FastTree; this function provides the means to do so.
+#' @returns The function returns a message that it has completed.
+#' @author John Quensen
+#' @export
+#' @examples 
+#' in.file <- system.file("extdata", "all_seq_complete.clust_rep_seqs.fasta", package="RDPutils")
+#' remove_model_seqs(in_file=in.file, out_file = file2tree.fasta)
+#' @keywords RDPTools
+
 remove_model_seqs <-
 function(in_file, out_file=in_file) {
   fasta <- readLines(in_file)
