0b642f49a47874cfa19d590d11ba52f6f453b38f
John Quensen <quensenj@msu.edu>
2022-09-18 22:20:01 -0400

    Roxygenize rename_fasta

--- R/rename_fasta.R
+++ R/rename_fasta.R
@@ -1,3 +1,26 @@
+#' @title Rename Representative Sequences
+#' @name rename_fasta
+#' @aliases rename_fasta
+#' @description This function renames representative sequences from their machine names to their OTU names.
+#' @usage rename_fasta(in_file = "names_trimmed.fasta", out_file = "renamed.fasta", rename_table)
+#' @param in_file The name of the fasta file containing representative sequences with their machine names.
+#' @param out_file The name of the fasta file containing representative sequences with their OTU names.
+#' @param rename_table A data frame associating the machine names of representative sequences with their corresponding OTU names.
+#' @details 
+#' The output of the function assoc_repseq_IDs_with_otus_by_fasta or assoc_repseq_IDs_with_otus_by_clstr  can be used as the rename_table.
+#' Before representative sequences can be renamed with this function, their IDs have to be shortened to include only their machine names with the function trim_fasta_names.
+#' @author John Quensen
+#' @export
+#' @seealso 
+#' [assoc_repseq_IDs_with_otus_by_clstr()]
+#' [assoc_repseq_IDs_with_otus_by_fasta()]
+#' [trim_fasta_names()]
+#' @examples 
+#' in.file <- system.file("extdata", "names_trimmed.fasta", package="RDPutils")
+#' data(assoc.table)
+#' rename_fasta(in_file = in.file, out_file = "renamed.fasta", rename_table = assoc.table)
+#' @keywords RDPTools
+
 rename_fasta <-
 function(in_file="names_trimmed.fasta", out_file="renamed.fasta", rename_table) {
   # Read in fasta file as text file.
